c19314ef74f3a8d46b2475ce891ac3558e19a4c5
nmccloud <nathan.mccloud898@gmail.com>
2023-01-02 15:43:59 -0600

    somewhat hackily recreating the example ui with tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex flex-col h-screen items-center justify-center">
-    <div class="setup-text">{{ setup }}</div>
-    <div v-if="showPunchlineClicked" class="punchline-text">{{ punchline }}</div>
-    <button class="punchline-button" @click="showPunchlineOrReset">{{ showPunchlineClicked ? 'Another' : 'Tell Me!' }}</button>
+    <div class="w-[240px] mr-[80px] setup-text p-4 rounded-2xl bg-teal-800 text-white">{{ setup }}</div>
+    <div v-if="showPunchlineClicked" class="w-[240px] ml-[80px] mt-2 p-4 rounded-2xl bg-red-800 text-white">{{ punchline }}</div>
+    <button class="w-[320px] shrink-0 bg-green rounded-lg py-2 hover:opacity-90 mt-4" @click="showPunchlineOrReset">{{ showPunchlineClicked ? 'Another' : 'Tell Me!' }}</button>
   </div>
 </template>
 
@@ -37,25 +37,4 @@ doSetup();
 </script>
 
 <style>
-.setup-text {
-  background-color: green;
-  color: white;
-  border-width: 10px;
-  border-color: green;
-}
-
-.punchline-text {
-  background-color: red;
-  color: white;
-  border-width: 10px;
-  border-color: red;
-}
-
-.punchline-button {
-  color:white;
-  border-width: 10px;
-  background-color: lightgray;
-  border-color: lightgray;
-}
-
 </style>
