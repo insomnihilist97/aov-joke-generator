00e7bdc187eaf963d05c0c0d1ea3c601b3c82a21
nmccloud <nathan.mccloud898@gmail.com>
2023-01-02 05:50:43 -0600

    working solution more or less

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,61 @@
 <template>
-  <div class="w-full h-full flex justify-center items-center">
-    <span class="text-3xl">Good luck!</span>
+  <div class="flex flex-col h-screen items-center justify-center">
+    <div class="setup-text">{{ setup }}</div>
+    <div v-if="showPunchlineClicked" class="punchline-text">{{ punchline }}</div>
+    <button class="punchline-button" @click="showPunchlineOrReset">{{ showPunchlineClicked ? 'Another' : 'Tell Me!' }}</button>
   </div>
 </template>
 
 <script setup>
-// fetch('https://v2.jokeapi.dev/joke/christmas') ...
+import { ref } from 'vue';
+
+const setup = ref(null);
+const punchline = ref(null);
+const showPunchlineClicked = ref(false)
+
+function doSetup() {
+  fetch('https://v2.jokeapi.dev/joke/christmas').then((response) => { 
+  response.json().then((res) => 
+  {
+    setup.value = res.setup;
+    punchline.value = res.delivery;
+  });
+})
+}
+
+function showPunchlineOrReset() {
+  if(!showPunchlineClicked.value)
+    showPunchlineClicked.value = true;
+  else {
+    doSetup();
+    showPunchlineClicked.value = false;
+  }
+}
+
+doSetup();
+
 </script>
+
+<style>
+.setup-text {
+  background-color: green;
+  color: white;
+  border-width: 10px;
+  border-color: green;
+}
+
+.punchline-text {
+  background-color: red;
+  color: white;
+  border-width: 10px;
+  border-color: red;
+}
+
+.punchline-button {
+  color:white;
+  border-width: 10px;
+  background-color: lightgray;
+  border-color: lightgray;
+}
+
+</style>
